1d0d71f3edd3542768d3d6a804bda43c9a26286e
Rena! <114522263+orbb4@users.noreply.github.com>
2025-11-28 08:04:59 -0300

    Add files via upload

---
+++ uhr-main/uhr_sa.cpp
@@ -0,0 +1,118 @@
+#include <cstdint>
+#include <chrono>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include "utils.cpp"
+
+
+// Include to be tested files here
+#include <sdsl/util.hpp>
+#include <sdsl/suffix_arrays.hpp>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include "../DocumentSa.cpp"
+
+using namespace sdsl;
+using namespace std;
+
+
+int main(int argc, char *argv[])
+{
+    // Validate and sanitize input
+    std::int64_t runs, lower, upper, step;
+    validate_input(argc, argv, runs, lower, upper, step);
+
+    // Set up clock variables
+    std::int64_t n, i, executed_runs;
+    std::int64_t total_runs_additive = runs * (((upper - lower) / step) + 1);
+    std::int64_t total_runs_multiplicative = runs * (floor(log(upper / double(lower)) / log(step)) + 1);
+    std::vector<double> times(runs);
+    std::vector<double> q;
+    double mean_time, time_stdev, dev;
+    auto begin_time = std::chrono::high_resolution_clock::now();
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::nano> elapsed_time = end_time - begin_time;
+
+    // Set up random number generation
+    std::random_device rd;
+    std::mt19937_64 rng(rd());
+    std::uniform_int_distribution<std::int64_t> u_distr; // change depending on app
+
+    // File to write time data
+    std::ofstream time_data;
+    time_data.open(argv[1]);
+    time_data << "n,t_mean,t_stdev,t_Q0,t_Q1,t_Q2,t_Q3,t_Q4" << std::endl;
+
+
+    // Begin testing
+    std::cerr << "\033[0;36mRunning tests...\033[0m" << std::endl << std::endl;
+    string text_file = "../pizzachili/englishV2.1024MB";
+    std::string pattern = "the";
+    csa_wt<> fm;
+
+    construct(fm, text_file, 1);
+    executed_runs = 0;
+    for (n = lower; n <= upper; n += step) {
+        mean_time = 0;
+        time_stdev = 0;
+        // Test configuration goes here
+        
+        //size_t pos = rand() % (text_file.size() - n);
+        //std::string pattern = text_file.substr(pos , n);
+
+
+        // Run to compute elapsed time
+        DocumentSuffixArray sa_index("../corpus_proteins.txt");
+        cout << "Tamaño del sa-index " << sa_index.size_in_mb() << " MB." << endl;
+
+        for (i = 0; i < runs; i++) {
+            // Remember to change total depending on step type
+
+            display_progress(++executed_runs, total_runs_additive);
+            memory_monitor::start();
+            begin_time = std::chrono::high_resolution_clock::now();
+            // Function to test goes here
+            sa_index.doc_locate("<?xml");
+            //DocumentFMIndex index("../corpus.txt");
+
+            
+            end_time = std::chrono::high_resolution_clock::now();
+            
+            
+            //node* x = fh.Create_node(rand() % n);
+            //H = fh.Insert(H, x);
+            elapsed_time = end_time - begin_time;
+            times[i] = elapsed_time.count();
+
+            mean_time += times[i];
+        }
+        // Compute statistics
+        mean_time /= runs;
+
+        for (i = 0; i < runs; i++) {
+            dev = times[i] - mean_time;
+            time_stdev += dev * dev;
+        }
+
+        time_stdev /= runs - 1; // Subtract 1 to get unbiased estimator
+        time_stdev = std::sqrt(time_stdev);
+
+        quartiles(times, q);
+
+        time_data << n << "," << mean_time << "," << time_stdev << ",";
+        time_data << q[0] << "," << q[1] << "," << q[2] << "," << q[3] << "," << q[4] << std::endl;
+    }
+
+    // This is to keep loading bar after testing
+    std::cerr << std::endl << std::endl;
+    std::cerr << "\033[1;32mDone!\033[0m" << std::endl;
+
+    time_data.close();
+
+    return 0;
+}
